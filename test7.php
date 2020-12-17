0055e215e89e783434d82c540a8fab8ae54d5d96
abiko23 <62099230+abiko23@users.noreply.github.com>
2020-12-17 18:09:39 +0900

    12/17

---
+++ test7.php
@@ -0,0 +1,8 @@
+<pre>
+<?php
+for ($i=1; $i<=365; $i++){
+  $day = date('n/j(D)', strtotime('+'.$i.'day'));
+  print($day."\n");
+}
+?>
+</pre>
---
+++ test8.php
@@ -0,0 +1,6 @@
+<?php
+$week_name = ['Sun', 'Mon', 'Tue','Wed', 'Thu', 'Fry', 'Sut'];
+
+print ('Today is ' .$week_name[date('w')]. 'day');
+
+?>
